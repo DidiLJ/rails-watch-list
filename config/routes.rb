8410d16567a4543a3a49118fbb4e8f7aa1e9ef3d
Adrien LOUIS-JOSEPH <louisjosephadrien@gmail.com>
2025-05-22 17:21:11 +0200

    watch list presque complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,15 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
+
+  # get '/lists/:id', to: 'lists#show', as: :list
+  # get '/movies', to: 'movies#index'
+  resources :movies, only: [:index]
+  resources :lists, only: [:index, :new, :show, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:show, :destroy]
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+require 'open-uri'
+require "json"
+require "open-uri"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.parse(url).read
+movies = JSON.parse(movies_serialized)["results"]
+
+Movie.destroy_all
+List.destroy_all
+
+movies.each do |movie|
+  movie = Movie.create!(
+    title: movie["original_title"],
+    overview: movie["overview"],
+    poster_url: movie["poster_path"],
+    rating: movie["vote_average"]
+  )
+  puts "Created #{movie.title}"
+end
+
+10.times do |i|
+  i = List.create!(
+    name: Faker::Book.title
+  )
+  puts "Created #{i.name} list"
+end
+
+puts "Finito pipo! #{Movie.count} movies created"
+puts "Finito pipo! #{List.count} lists created"
